81a3084ee31425c13814361b48f84e538b7b0e68
Damiano Racic <damianoracic06@gmail.com>
2026-06-27 12:09:49 -0700

    first complete iteration

--- sortDownloads.py
+++ sortDownloads.py
@@ -4,7 +4,7 @@ import shutil
 downloads = Path.home() / "Downloads"
 
 CATEGORIES = {
-    "Images": [".jpg", ".jpeg", ",png", ".gif", "webp"],
+    "Images": [".jpg", ".jpeg", ".png", ".gif", ".webp"],
     "PDFs": [".pdf"],
     "Docs": [".docx"],
     "TextFiles": [".txt"],
@@ -18,6 +18,11 @@ def get_category(suffix):
     for folder_name, extensions in CATEGORIES.items():
         if suffix in extensions:
             return folder_name
+    return "Other"
         
-
-            
+for item in downloads.iterdir():
+    if item.is_file():
+        category = get_category(item.suffix.lower())
+        destination_folder = downloads / category
+        destination_folder.mkdir(exist_ok = True) #Creates folder if needed
+        shutil.move(str(item), destination_folder / item.name)
